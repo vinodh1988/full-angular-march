9862e106dee5b79cc168530c7cb34442d16c8f02
vinodh1988 <k.c.vinodh@hotmail.com>
2025-03-06 11:45:42 +0530

    login

--- src/app/common/login/login.component.ts
+++ src/app/common/login/login.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  login(){
+ 
+    if(this.username=="peter" && this.password
+    =="password"){
+  
+      localStorage.setItem("username",this.username)
+      window.location.reload()
+  }
+  else alert("Invalid Credentials")
+}
 
+logout(){
+  localStorage.removeItem("username")
+  window.location.reload()
+}
 }
--- src/app/common/menu-bar/menu-bar.component.ts
+++ src/app/common/menu-bar/menu-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-bar',
@@ -8,11 +9,21 @@ import { Component } from '@angular/core';
 export class MenuBarComponent {
   status:boolean=false;
   loginmenu:string="Login"
-     constructor(){
+     constructor(private router:Router){
        let username = localStorage.getItem("username")
         if(username){
           this.status=true;
           this.loginmenu=username+", Logout"
         }
      }
+
+     loginhandler(){
+        if(this.status){
+          localStorage.removeItem("username")
+          window.location.reload()
+        }
+        else{
+          this.router.navigate(['/login'])
+     }
+}
 }
